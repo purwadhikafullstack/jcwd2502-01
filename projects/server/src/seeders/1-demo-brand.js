448988a5e0816a8a4451151e9fb1d91873b75e05
Albert Santoso <albertsantosotandjung@gmail.com>
2023-10-17 15:44:15 +0700

    chore: setup server and formatting file

--- projects/server/src/seeders/1-demo-brand.js
+++ projects/server/src/seeders/1-demo-brand.js
@@ -2,36 +2,36 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('brands', [
-	{
-      "id": 1,
-      "brand_name": "Logitech"
-    },
-    {
-      "id": 2,
-      "brand_name": "Razer"
-    },
-    {
-      "id": 3,
-      "brand_name": "Steelseries"
-    },
-    {
-      "id": 4,
-      "brand_name": "Corsair"
-    },
-    {
-      "id": 5,
-      "brand_name": "Fantech"
-    },
-    {
-      "id": 6,
-      "brand_name": "AOC"
-    }
-	], {});
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert('brands', [
+			{
+				"id": 1,
+				"brand_name": "Logitech"
+			},
+			{
+				"id": 2,
+				"brand_name": "Razer"
+			},
+			{
+				"id": 3,
+				"brand_name": "Steelseries"
+			},
+			{
+				"id": 4,
+				"brand_name": "Corsair"
+			},
+			{
+				"id": 5,
+				"brand_name": "Fantech"
+			},
+			{
+				"id": 6,
+				"brand_name": "AOC"
+			}
+		], {});
+	},
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('brands', null, {});
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('brands', null, {});
+	}
 };
