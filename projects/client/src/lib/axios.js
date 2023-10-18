e6c11ee73cdc852474b9a4f591b85ad8bb8c10c7
rafaeltriputra <rafaeltriputra17@gmail.com>
2023-10-18 22:48:19 +0700

    connect FE BE

--- projects/client/src/lib/axios.js
+++ projects/client/src/lib/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 function ApiInstance(authorization = null) {
-    return axios.create({
-        baseURL: import.meta.env.VITE_REACT_APP_URL,
-        headers: {
-            authorization,
-        },
-    });
+	return axios.create({
+		baseURL: import.meta.env.REACT_APP_API_BASE_URL,
+		headers: {
+			authorization,
+		},
+	});
 }
 
 export const axiosInstance = ApiInstance;
