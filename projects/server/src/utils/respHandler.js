e6c435c0b3ededfd4f107b426e0892686dacfe6e
rafaeltriputra <rafaeltriputra17@gmail.com>
2023-10-18 16:33:18 +0700

    get data products done

--- projects/server/src/utils/respHandler.js
+++ projects/server/src/utils/respHandler.js
@@ -2,7 +2,7 @@ const respHandler = (res, message, data, status, isError) => {
 	return res.status(status || 200).send({
 		isError: isError || false,
 		message,
-		data,
+		data: data || null,
 	});
 };
 
