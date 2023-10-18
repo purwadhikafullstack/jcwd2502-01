67231c961e6774f0c743660730a07d826913ce8d
Andrean <andrean923@gmail.com>
2023-10-18 20:25:08 +0700

    register setup

--- projects/server/src/controllers/usersController.js
+++ projects/server/src/controllers/usersController.js
@@ -3,6 +3,7 @@ const fs = require("fs").promises;
 const handlebars = require("handlebars");
 const transporter = require("./../helper/transporter");
 const { createUser } = require("./../services/usersService");
+const respHandler = require("./../utils/respHandler");
 
 module.exports = {
 	register: async (req, res, next) => {
@@ -11,10 +12,15 @@ module.exports = {
 			console.log(username);
 			const getUsername = await createUser(req.body);
 			console.log(getUsername);
-			res.send(getUsername);
+			if (getUsername.message) {
+				respHandler(res, getUsername.message, true);
+			} else {
+				respHandler(res, "Register Successed");
+			}
+			// res.send(getUsername);
 		} catch (error) {
 			console.log(error);
-			res.send(error);
+			next(error);
 		}
 	},
 };
--- projects/server/src/services/usersService.js
+++ projects/server/src/services/usersService.js
@@ -1,4 +1,10 @@
 const db = require("./../models");
+const fs = require("fs").promises;
+const handlebars = require("handlebars");
+const path = require("path");
+const transporter = require("./../helper/transporter");
+const { createJWT } = require("./../lib/jwt");
+const { hash, match } = require("./../helper/hashing");
 
 module.exports = {
 	createUser: async (body) => {
@@ -8,10 +14,38 @@ module.exports = {
 				where: { username },
 			});
 			if (checkUsername) return { message: "username already used" };
-			const checkEmail = await db.user.findOne({ where: { email } });
-			if (checkEmail) return { message: "email already used" };
-			// return body;
+			// const checkEmail = await db.user.findOne({ where: { email } });
+			// if (checkEmail) return { message: "email already used" };
+			const hashPassword = await hash(password);
+			console.log(hashPassword);
+			const registerUser = await db.user.create({
+				username,
+				email,
+				password: hashPassword,
+				role,
+				status: "inactive",
+			});
+
+			const token = createJWT({ id: registerUser.dataValues.id }, "1h");
+			const readTemplate = await fs.readFile(
+				path.join(__dirname, "../public/index.html"),
+				"utf-8"
+			);
+			const compiledTemplate = await handlebars.compile(readTemplate);
+			const newTemplate = compiledTemplate({ username, token });
+
+			await transporter.sendMail({
+				from: {
+					name: "nexocomp",
+					email: "nexocomppurwadhika@gmail.com",
+				},
+				to: email,
+				subject: "Register New Account",
+				html: newTemplate,
+			});
+			return true;
 		} catch (error) {
+			console.log(error);
 			return error;
 		}
 	},
