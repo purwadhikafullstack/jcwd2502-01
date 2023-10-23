186375f40bbd5e429cf79ae005bf829515b18dab
Jordan <74591126+jordansmrd@users.noreply.github.com>
2023-10-23 15:04:31 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8002,
+        PORT: 2501,
       },
       time: true,
     },
