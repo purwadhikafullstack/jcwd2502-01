4eaf7a48f6bad50116ca4ea06ed3e4429d71a2f7
Albert Santoso <albertsantosotandjung@gmail.com>
2023-11-13 14:30:45 +0700

    chore: add admin/stocks page

--- projects/client/src/pages/user/OrderListPage.jsx
+++ projects/client/src/pages/user/OrderListPage.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const OrderListPage = () => {
 	return (
-		<main className="my-container min-h-screen">
-			<h1>Transaction History</h1>
+		<main className="my-container min-h-screen py-4">
+			<div className="page-heading mb-4">
+				<h3 className="font-bold text-headline-sm">
+					Transaction History
+				</h3>
+			</div>
+			<div className="order-list">
+				<div className="order-card"></div>
+			</div>
 		</main>
 	);
 };
