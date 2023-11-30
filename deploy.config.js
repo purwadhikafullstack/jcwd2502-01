781eeef1b15eb1630df54963180f4462677989bf
ridhozhr10 <azharridho42@gmail.com>
2023-11-30 17:04:36 +0700

    fix: gambar

--- deploy.config.js
+++ deploy.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "JCWD-2502-01", // Format JCWD-{batchcode}-{groupnumber}
-            script: "./projects/server/src/index.js",
+            script: "./src/index.js",
             env: {
                 NODE_ENV: "production",
                 PORT: 2501,
