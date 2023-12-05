00e9fd06f75c5ba463db0beab2b53600dd7f1f4f
rafaeltriputra <rafaeltriputra17@gmail.com>
2023-12-05 18:44:27 +0700

    bug fix add product

--- projects/server/src/services/productsService.js
+++ projects/server/src/services/productsService.js
@@ -197,6 +197,7 @@ module.exports = {
 				);
 			}
 
+			dataSpec.product_id = addProduct.dataValues.id;
 			const addProductSpec = await db.specification.create(dataSpec, {
 				transaction: t,
 			});
